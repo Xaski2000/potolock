ae6f9a6163030827a7d9729283ef4ba32c9ebd78
Ivan Shashkov <mr.xaski.2002@gmail.com>
2021-02-12 19:16:06 +0700

    prototype menu

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
 $('#color-picker').spectrum({
 	type: "component",
 	preferredFormat: "hex",
-	showInput: true,
-	showInitial: true,
 	showAlpha: false,
 	allowEmpty: true,
 	showPalette: true,
 	showSelectionPalette: true,
 	palette: [],
 	maxSelectionSize: 22,
-	cancelText: "Отмена",
-	chooseText: "Выбрать",
 	color: "#ffffff",
+	showInitial: true,
+   showInput: true,
+	showButtons: false,
 	move: function (color) {
 		let a = color.toHexString();
 		// document.getElementsByClassName("color_layer-path").style.fill = a;
@@ -23,8 +22,6 @@ $('#color-picker').spectrum({
 $('#color-picker-for-wall').spectrum({
 	type: "component",
 	preferredFormat: "hex",
-	showInput: true,
-	showInitial: true,
 	showAlpha: false,
 	allowEmpty: true,
 	showPalette: true,
@@ -34,6 +31,9 @@ $('#color-picker-for-wall').spectrum({
 	cancelText: "Отмена",
 	chooseText: "Выбрать",
 	color: "#ffffff",
+	showInitial: true,
+   showInput: true,
+	showButtons: false,
 	move: function (color) {
 		let a = color.toHexString();
 		// document.getElementsByClassName("color_layer-path").style.fill = a;
@@ -45,8 +45,6 @@ $('#color-picker-for-wall').spectrum({
 $('#color-picker-for-cover').spectrum({
 	type: "component",
 	preferredFormat: "hex",
-	showInput: true,
-	showInitial: true,
 	showAlpha: false,
 	allowEmpty: true,
 	showPalette: true,
@@ -56,6 +54,10 @@ $('#color-picker-for-cover').spectrum({
 	cancelText: "Отмена",
 	chooseText: "Выбрать",
 	color: "#ffffff",
+	showInitial: true,
+   showInput: true,
+	showButtons: false,
+	disabled: true,
 	move: function (color) {
 		let a = color.toHexString();
 		// document.getElementsByClassName("color_layer-path").style.fill = a;
@@ -64,63 +66,9 @@ $('#color-picker-for-cover').spectrum({
 	}
 });
 
-// let width = $(".wrapper").css('width');
-// console.log(width);
-
-
-
-// var target = document.getElementById('wrapper');
-
-// // Конфигурация observer (за какими изменениями наблюдать)
-// const config = {
-// 	attributes: true,
-// 	childList: true,
-// 	subtree: true
-// };
-
-// // Функция обратного вызова при срабатывании мутации
-// const callback = function (mutationsList, observer) {
-// 	for (let mutation of mutationsList) {
-// 		if (mutation.type === 'attributes') {
-// 			console.log('The ' + mutation.attributeName + ' attribute was modified.');
-// 		}
-// 	}
-// };
-
-// // Создаем экземпляр наблюдателя с указанной функцией обратного вызова
-// const observer = new MutationObserver(callback);
-
-// // Начинаем наблюдение за настроенными изменениями целевого элемента
-// observer.observe(target, config);
-
-
-$('#light-dots-button').on('click', function () {
-	if ($(".light-dots").css("display") == 'none') {
-		$(".light-dots").css("display", "inline-block");
-		$(".light-dots-button").css("background-color", "#337788");
-	} else {
-		$(".light-dots").css("display", "none");
-		$(".light-dots-button").css("background-color", "#773388");
-	}
-});
-
-$('#light-button').on('click', function () {
-	if ($(".light").css("display") == 'none') {
-		$(".light").css("display", "inline-block");
-		$(".light-button").css("background-color", "#337788");
-	} else {
-		$(".light").css("display", "none");
-		$(".light-button").css("background-color", "#773388");
-	}
-});
-
-$('#furniture-button').on('click', function () {
-	if ($(".furniture").css("display") == 'none') {
-		$(".furniture").css("display", "inline-block");
-		$(".furniture-button").css("background-color", "#337788");
-	} else {
-		$(".furniture").css("display", "none");
-		$(".furniture-button").css("background-color", "#773388");
+$('#color-picker-for-cover').on('click', function () {
+	if ($(".cover").css("display") == 'none') {
+		
 	}
 });
 
@@ -128,11 +76,44 @@ $('#cover-button').on('click', function () {
 	if ($(".cover").css("display") == 'none') {
 		$(".cover").css("display", "inline-block");
 		$(".cover-color").css("display", "inline-block");
-		$(".cover-button").css("background-color", "#337788");
+		$(".cover-button").css("background-color", "#bbb");
+		$("#color-picker-for-cover").spectrum("enable");
+
 	} else {
 		$(".cover").css("display", "none");
 		$(".cover-color").css("display", "none");
-		$(".cover-button").css("background-color", "#773388");
+		$(".cover-button").css("background-color", "#999");
+		$("#color-picker-for-cover").spectrum("disable");
+	}
+});
+
+$('#light-dots-button').on('click', function () {
+	if ($(".light-dots").css("display") == 'none') {
+		$(".light-dots").css("display", "inline-block");
+		$(".light-dots-button").css("background-color", "#bbb");
+	} else {
+		$(".light-dots").css("display", "none");
+		$(".light-dots-button").css("background-color", "#999");
+	}
+});
+
+$('#light-button').on('click', function () {
+	if ($(".light").css("display") == 'none') {
+		$(".light").css("display", "inline-block");
+		$(".light-button").css("background-color", "#bbb");
+	} else {
+		$(".light").css("display", "none");
+		$(".light-button").css("background-color", "#999");
+	}
+});
+
+$('#furniture-button').on('click', function () {
+if ($(".furniture").css("display") == 'none') {
+	$(".furniture").css("display", "inline-block");
+		$(".furniture-button").css("background-color", "#bbb");
+	} else {
+		$(".furniture").css("display", "none");
+		$(".furniture-button").css("background-color", "#999");
 	}
 });
 
@@ -140,10 +121,40 @@ $('#radio-button').on('click', function () {
 	if ($(".radio").css("display") == 'none') {
 		$(".radio").css("display", "inline-block");
 		$(".radio-color").css("display", "inline-block");
-		$(".radio-button").css("background-color", "#337788");
+		$(".radio-button").css("background-color", "#bbb");
 	} else {
 		$(".radio").css("display", "none");
 		$(".radio-color").css("display", "none");
-		$(".radio-button").css("background-color", "#773388");
+		$(".radio-button").css("background-color", "#999");
 	}
 });
+
+
+	// let width = $(".wrapper").css('width');
+	// console.log(width);
+
+
+
+	// var target = document.getElementById('wrapper');
+
+	// // Конфигурация observer (за какими изменениями наблюдать)
+	// const config = {
+	// 	attributes: true,
+	// 	childList: true,
+	// 	subtree: true
+	// };
+
+	// // Функция обратного вызова при срабатывании мутации
+	// const callback = function (mutationsList, observer) {
+	// 	for (let mutation of mutationsList) {
+	// 		if (mutation.type === 'attributes') {
+	// 			console.log('The ' + mutation.attributeName + ' attribute was modified.');
+	// 		}
+	// 	}
+	// };
+
+	// // Создаем экземпляр наблюдателя с указанной функцией обратного вызова
+	// const observer = new MutationObserver(callback);
+
+	// // Начинаем наблюдение за настроенными изменениями целевого элемента
+	// observer.observe(target, config);
